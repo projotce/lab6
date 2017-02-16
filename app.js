32636d9c2ae92a1b6addbd252fc4532fbd006fae
Pooja Ekbote <pekbote@ucsd.edu>
2017-02-16 09:38:18 -0800

    ajax lab

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
+var palette = require('./routes/palette');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,8 +39,10 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, addProject);
+	console.log("/project/" + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] 
+		+ '" class="detailsImage">' + '<p>'
+		+ result['title'] + '</p><small>' + result['date'] 
+		+ result['summary'] + '</small>';
+	$("#project" + result['id']+ " .details").html(projectHTML);
+
 }
 
 /*
@@ -35,4 +47,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
+}
+
+function addColor(result) {
+	console.log(result);
+	var colors = (result['colors']).hex;
+	console.log(colors);
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
